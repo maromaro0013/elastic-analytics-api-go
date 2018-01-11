657b5494cd5a9123a9cac5a0bab7795a7e7e6d23
maromaro0013 <ota0120@gmail.com>
2018-01-11 12:03:30 +0900

    indexのリストが取得できるようにした

--- src/elastic-analytics-api/main.go
+++ src/elastic-analytics-api/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+  "context"
   "fmt"
   "log"
-  "os"
+  //"os"
 
   // "net/http"
   // "goji.io"
   // "goji.io/pat"
 
   "github.com/joho/godotenv"
+  elastic "gopkg.in/olivere/elastic.v5"
 )
 
 /*
@@ -25,8 +27,26 @@ func main() {
     log.Fatal("Error loading .env file")
   }
 
-  es_host := os.Getenv("ELASTICSEARCH_HOST00")
-  fmt.Println(es_host)
+  //es_host := os.Getenv("ELASTICSEARCH_HOST00")
+
+  client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"), elastic.SetSniff(false))
+  if err != nil {
+    log.Fatalf("Connect failed: %v", err)
+  }
+  defer client.Stop()
+  log.Print("Connected")
+
+  res, err := client.ClusterState().Metric("version").Do(context.Background())
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("Cluster %q has nodes %d", res.ClusterName, res.Version)
+
+  names, err := client.IndexNames()
+  for _, name := range names {
+    fmt.Println(name)
+  }
+
 /*
   mux := goji.NewMux()
   mux.HandleFunc(pat.Get("/test/:name"), test)
